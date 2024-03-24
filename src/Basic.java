ea9720cfe43e8c5642ec4c5edae8c44c6d3823e0
Gongjjin <gjyj5395@naver.com>
2024-03-24 22:55:46 +0900

    commit message

---
+++ src/Basic.java
@@ -0,0 +1,20 @@
+import javax.print.CancelablePrintJob;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[][] answer = {};
+        int x =1;
+        for(int i=0; i<a;i++){
+
+            for(int j=0; j<a; j++){
+
+            }
+        }
+        }
+
+    }
+
---
+++ src/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        return answer;
+        //실패율 : 스테이지도달, 클리어 못한 수 / 스테이지 도달 플레이어 수
+        
+    }
+}
